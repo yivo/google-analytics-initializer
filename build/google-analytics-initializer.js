d7a5bdae94f05e2f46d51e85d007764dc1afcefd
Yaroslav Konoplov <eahome00@gmail.com>
2017-02-06 15:28:43 +0200

    Manually ga('set', 'page')
    Signed-off-by: Yaroslav Konoplov <eahome00@gmail.com>

--- build/google-analytics-initializer.js
+++ build/google-analytics-initializer.js
@@ -1,55 +1,51 @@
 
 /*!
- * google-analytics-initializer 1.0.6 | https://github.com/yivo/google-analytics-initializer | MIT License
+ * google-analytics-initializer 1.0.7 | https://github.com/yivo/google-analytics-initializer | MIT License
  */
 
 (function() {
   var el, head, i, initialize, len1, ref, trackingID;
 
-  initialize = (function() {
-    var initialized;
-    initialized = false;
-    return function(trackingID) {
-      var pageview, ref, ref1, script;
-      if (!initialized) {
-        if (!trackingID) {
-          throw new TypeError('[Google Analytics Initializer] Tracking ID is required');
-        }
-        window.GoogleAnalyticsObject = 'ga';
-        window[window.GoogleAnalyticsObject] = function() {
-          var args, idx, len;
-          args = [];
-          len = arguments.length;
-          idx = -1;
-          while (++idx < len) {
-            args.push(arguments[idx]);
-          }
-          (ga.q != null ? ga.q : ga.q = []).push(args);
-        };
-        ga.l = (ref = typeof Date.now === "function" ? Date.now() : void 0) != null ? ref : +new Date();
-        script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.async = true;
-        script.src = 'https://www.google-analytics.com/analytics.js';
-        if ((ref1 = document.getElementsByTagName('head')[0]) != null) {
-          ref1.appendChild(script);
-        }
-        ga('create', trackingID, 'auto');
-        pageview = function() {
-          return ga('send', 'pageview', location.href.split('#')[0]);
-        };
-        if (typeof Turbolinks !== "undefined" && Turbolinks !== null ? Turbolinks.supported : void 0) {
-          $(document).on('page:change', pageview);
-        } else {
-          pageview();
-          if ($.support.pjax) {
-            $(document).on('pjax:end', pageview);
-          }
-        }
-        initialized = true;
+  initialize = function(trackingID) {
+    var $document, pageset, pageview, ref, script;
+    if (!trackingID) {
+      throw new TypeError('[Google Analytics Initializer] Tracking ID is required');
+    }
+    window.GoogleAnalyticsObject = 'ga';
+    window[window.GoogleAnalyticsObject] = function() {
+      var args, idx, len;
+      args = [];
+      len = arguments.length;
+      idx = -1;
+      while (++idx < len) {
+        args.push(arguments[idx]);
       }
+      return (ga.q != null ? ga.q : ga.q = []).push(args);
     };
-  })();
+    ga.l = (ref = typeof Date.now === "function" ? Date.now() : void 0) != null ? ref : +new Date();
+    script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = 'https://www.google-analytics.com/analytics.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
+    ga('create', trackingID, 'auto');
+    pageset = function() {
+      return ga('set', 'page', location.href.split('#')[0]);
+    };
+    pageview = function() {
+      return ga('send', 'pageview');
+    };
+    if (typeof Turbolinks !== "undefined" && Turbolinks !== null ? Turbolinks.supported : void 0) {
+      $document = $(document);
+      $document.one('page:change', function() {
+        return $document.on('page:change', function() {
+          pageset();
+          return pageview();
+        });
+      });
+    }
+    return pageview();
+  };
 
   if ((head = document.getElementsByTagName('head')[0]) != null) {
     ref = head.getElementsByTagName('meta');
--- source/google-analytics-initializer.coffee
+++ source/google-analytics-initializer.coffee
@@ -1,48 +1,45 @@
 ###!
-# google-analytics-initializer 1.0.6 | https://github.com/yivo/google-analytics-initializer | MIT License
+# google-analytics-initializer 1.0.7 | https://github.com/yivo/google-analytics-initializer | MIT License
 ###
   
-initialize = do ->
-  initialized = no
+initialize = (trackingID) ->
+  unless trackingID
+    throw new TypeError('[Google Analytics Initializer] Tracking ID is required')    
 
-  (trackingID) ->
-    unless initialized    
-      unless trackingID
-        throw new TypeError('[Google Analytics Initializer] Tracking ID is required')    
-    
-      window.GoogleAnalyticsObject         = 'ga'
-      window[window.GoogleAnalyticsObject] = ->
-        args = []
-        len  = arguments.length
-        idx  = -1
-        args.push(arguments[idx]) while ++idx < len
-        (ga.q ?= []).push(args)
-        return
-  
-      ga.l = Date.now?() ? +new Date()
-  
-      script       = document.createElement('script')
-      script.type  = 'text/javascript'
-      script.async = true
-  
-      # Use HTTPS to download script. Google redirects from HTTP to HTTPS.
-      # This will save us from one redirect.
-      script.src   = 'https://www.google-analytics.com/analytics.js'
-  
-      document.getElementsByTagName('head')[0]?.appendChild(script)
-  
-      ga('create', trackingID, 'auto')
-  
-      pageview = -> ga('send', 'pageview', location.href.split('#')[0])
-  
-      if Turbolinks?.supported
-        $(document).on('page:change', pageview)
-      else
+  window.GoogleAnalyticsObject         = 'ga'
+  window[window.GoogleAnalyticsObject] = ->
+    args = []
+    len  = arguments.length
+    idx  = -1
+    args.push(arguments[idx]) while ++idx < len
+    (ga.q ?= []).push(args)
+
+  ga.l = Date.now?() ? +new Date()
+
+  script       = document.createElement('script')
+  script.type  = 'text/javascript'
+  script.async = true
+
+  # Use HTTPS to download script. Google redirects from HTTP to HTTPS.
+  # This will save us from one redirect.
+  script.src   = 'https://www.google-analytics.com/analytics.js'
+
+  document.getElementsByTagName('head')[0].appendChild(script)
+
+  ga('create', trackingID, 'auto')
+
+  pageset  = -> ga('set', 'page', location.href.split('#')[0])
+  pageview = -> ga('send', 'pageview')
+
+  # https://developers.google.com/analytics/devguides/collection/analyticsjs/single-page-applications
+  if Turbolinks?.supported
+    $document = $(document)
+    $document.one 'page:change', ->
+      $document.on 'page:change', ->
+        pageset()
         pageview()
-        $(document).on('pjax:end', pageview) if $.support.pjax
 
-      initialized = true
-    return
+  pageview()
 
 if (head = document.getElementsByTagName('head')[0])?
   for el in head.getElementsByTagName('meta')
